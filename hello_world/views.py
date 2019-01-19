6bbbe8942fb48177fde8b095c3cc2c29224becca
ale.kacperczyk <ale.kacperczyk@gmail.com>
2019-01-19 14:47:35 +0100

    Lint bugs fixed

--- hello_world/views.py
+++ hello_world/views.py
@@ -6,6 +6,7 @@ from flask import request
 moje_imie = "Natalia"
 msg = "Hello World!"
 
+
 @app.route('/')
 def index():
     output = request.args.get('output')
@@ -13,6 +14,7 @@ def index():
         output = PLAIN
     return get_formatted(msg, moje_imie,
                          output.lower())
+
 
 @app.route('/outputs')
 def supported_output():
