07737e9480be6bb73bc6d59e9138695122d7c36a
student <tester@tester-m>
2019-06-08 07:58:47 +0000

    zmiana imienia

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from formater import get_formatted
 from formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Natalia"
+moje_imie = "Slawek"
 msg = "Hello World!"
 
 @app.route('/')
