eb1f605ae0fff55468523d3eb1349d1a560ad5aa
dorsplitt <brzykcydor@gmail.com>
2020-02-01 20:20:21 +0100

    xml

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -3,6 +3,8 @@ PLAIN = "plain"
 PLAIN_UP = "plain_uppercase"
 PLAIN_LO = "plain_lowercase"
 JSON = "json"
+XML = "xml"
+
 
 SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON]
 
@@ -17,7 +19,13 @@ def get_formatted(msg, imie, format):
         result = plain_text_lower_case(msg, imie)
     elif format == JSON:
         result = format_to_json(msg, imie)
+    elif format == XML:
+        result = format_to_xml(msg, imie)
     return result
+
+
+def format_to_xml(msg, imie):
+    return ("<grettings>" + "<name>" + imie + "</name>" + "<msg>" + msg + "</msg>" + "</greetings>")
 
 
 def format_to_json(msg, imie):
