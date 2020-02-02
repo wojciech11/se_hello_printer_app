86677e54c0fab85f8d4c71f02713f9be95ffcf1e
cedrospl <[hagnar93@gmail.com]>
2020-02-02 13:46:03 +0100

    XMLoutputAdded

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -3,6 +3,7 @@ PLAIN = "plain"
 PLAIN_UP = "plain_uppercase"
 PLAIN_LO = "plain_lowercase"
 JSON = "json"
+XML = "xml"
 
 SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON]
 
@@ -17,6 +18,8 @@ def get_formatted(msg, imie, format):
         result = plain_text_lower_case(msg, imie)
     elif format == JSON:
         result = format_to_json(msg, imie)
+    elif format == XML:
+        result = format_to_xml(msg, imie)
     return result
 
 
@@ -24,6 +27,9 @@ def format_to_json(msg, imie):
     return ('{ "imie":"' + imie + '", "mgs":' +
             msg + '"}')
 
+def format_to_xml(msg, imie):
+    return ('{ "imie":"' + imie + '", "mgs":' +
+            msg + '"}')
 
 def plain_text(msg, imie):
     return imie + ' ' + msg
