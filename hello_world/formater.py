7af69db686c16492f94aa719c7b5082a40355b31
Karol-tester <bukowiecki.karol@gmail.com>
2020-02-02 13:54:28 +0100

    add ml

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -3,8 +3,9 @@ PLAIN = "plain"
 PLAIN_UP = "plain_uppercase"
 PLAIN_LO = "plain_lowercase"
 JSON = "json"
+XML="xml"
 
-SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON]
+SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON,XML]
 
 
 def get_formatted(msg, imie, format):
@@ -17,6 +18,8 @@ def get_formatted(msg, imie, format):
         result = plain_text_lower_case(msg, imie)
     elif format == JSON:
         result = format_to_json(msg, imie)
+    elif format == XML:
+        result = format_to_xml(msg, imie)
     return result
 
 
@@ -24,6 +27,11 @@ def format_to_json(msg, imie):
     return ('{ "imie":"' + imie + '", "msg":' +
             msg + '"}')
 
+def format_to_xml(msg, imie):
+    return ("""<greetings>
+    <name> {0} </name>
+    <msg> {1} </msg>
+</greetings>""".format(imie,msg)+ "\n")
 
 def plain_text(msg, imie):
     return imie + ' ' + msg
