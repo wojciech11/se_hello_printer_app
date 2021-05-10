30ddd2e585c23cf463397c1ab3f7bd579351b661
student <KatarzynaMyrcik@users.noreply.github.com>
2021-05-10 04:58:06 +0000

    poprawka skladni, lint

--- test/test_views.py
+++ test/test_views.py
@@ -1,7 +1,6 @@
 import unittest
 from hello_world import app
 from hello_world.formater import SUPPORTED
-import json
 
 
 class FlaskrTestCase(unittest.TestCase):
@@ -14,14 +13,15 @@ class FlaskrTestCase(unittest.TestCase):
         s = str(rv.data)
         ','.join(SUPPORTED) in s
 
-    def test_msg_with_output(self):
-        rv = self.app.get('/?output=json')
+#    def test_msg_with_output(self):
+#        rv = self.app.get('/?output=json')
 #        test_data = {"imie": "Katarzyna", "msg": "Hello World!"}
 #        y=json.loads(rv.data)
 #        self.assertEqual(b'{test_data["msg"], y:["msg"]}', rv.data)
 #        self.assertEqual(b'{test_data["imie"], y:["imie"]}', rv.data)
-        self.assertEqual(b'{ "imie":"Katarzyna", "mgs":"Hello World!"}', rv.data) # noqa
-#test ktory sprawdza ilosc pol
+#        self.assertEqual(b'{ "imie":"Katarzyna", "mgs":"Hello World!"}', rv.data) # noqa
+#       test ktory sprawdza ilosc pol
+
     def test_msg_with_output(self):
         rv = self.app.get('/?output=xml')
         self.assertEqual(b'<greetings><name>' + b'Katarzyna' + b'</name><msg>' + b'Hello World!' + b'</msg></greetings>', rv.data) # noqa
