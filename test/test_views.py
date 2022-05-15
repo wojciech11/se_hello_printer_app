ec24ad0e820b8928a36649535fc9629ae511cbad
najgorzata <najgorzata@users.noreply.github.com>
2022-05-15 09:43:26 +0000

    zmiana imienia

--- test/test_views.py
+++ test/test_views.py
@@ -15,4 +15,4 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Natalia", "mgs":Hello World!"}', rv.data)
+        self.assertEqual(b'{ "imie":"Gosia", "mgs":Hello World!"}', rv.data)
