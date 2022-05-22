03a62f2ac0b1c02948028ac4a433b5c7c7ac7785
rpodsiadly <rpodsiadly@users.noreply.github.com>
2022-05-22 11:24:44 +0200

    powrot

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "ŁĄŚ"
+moje_imie = "Robert"
 msg = "Hello World!"
 
 
