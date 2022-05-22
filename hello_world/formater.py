bc41516cb682e88437d55ea06400c817b2daf70e
rpodsiadly <rpodsiadly@users.noreply.github.com>
2022-05-22 09:03:31 +0200

    json bug fixed, tests fixed

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -22,7 +22,7 @@ def get_formatted(msg, imie, format):
 
 def format_to_json(msg, imie):
     return ('{ "imie":"' + imie + '", "mgs":' +
-            msg + '"}')
+            '"'+msg + '"}')
 
 
 def plain_text(msg, imie):
